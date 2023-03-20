aad4964f4c6efcf69cb9daf990c035d8fec3c64d
eart4 <metrika987@gmail.com>
2023-03-20 18:00:33 +0300

    added media

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-        dots: false,
-        margin: 48,
-        loop: true
+
+		responsive: {
+			// breakpoint from 0 up
+			loop: true,
+			0: {
+				items: 2,
+				margin: 32,
+				loop: true,
+				dots: false,
+			},
+			// breakpoint from 480 up
+			601: {
+				items: 3,
+				margin: 32,
+				loop: true,
+				dots: false,
+			},
+			// breakpoint from 768 up
+			1001: {
+				items: 2,
+				margin: 48,
+				dots: false,
+				loop: true,
+			},
+            1835: {
+                items: 3,
+            }
+
+        }
     });
   });
 
